9fe27a5f3e34820169f6e64ac45ee9a2a4b8e0ca
Dainis Dilevka <thedenisovan@proton.me>
2025-12-30 10:43:21 +0000

    add: seperate ts.config files for dev and production

--- server/build/app.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const app = express();
-app.get('/', (req, res) => {
-    res.json({
-        greeting: 'Hello World!',
-    });
-});
-export default app;
-//# sourceMappingURL=app.js.map
--- server/build/server.js
+++
@@ -1,5 +0,0 @@
-import app from './app.js';
-app.listen(5000, () => {
-    console.log(`Server running on port 5000`);
-});
-//# sourceMappingURL=server.js.map
