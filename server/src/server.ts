c2a93dfca8ce5e51902776e9d466ce5c68d7e3f5
Dainis Dilevka <thedenisovan@proton.me>
2025-12-31 10:22:47 +0000

    ny commit

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,8 @@
 import app from './app.js';
+import 'dotenv/config';
 
-app.listen(5000, () => {
-  console.log(`Server running on port 5000`);
+const PORT = process.env['PORT'] || 5000;
+
+app.listen(+PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
