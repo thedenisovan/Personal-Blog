b899c205a2ec04e2e9a5c1afcbc961ba69372448
Dainis Dilevka <thedenisovan@proton.me>
2026-01-11 13:54:21 +0000

    edit: changed user token expire from 30m to 1h

--- server/src/middleware/jwtSign.ts
+++ server/src/middleware/jwtSign.ts
@@ -14,7 +14,7 @@ export default function jwtSign(req: Request, res: Response) {
   jwt.sign(
     user,
     process.env.ACCESS_TOKEN_SECRET as string,
-    { expiresIn: '30m' },
+    { expiresIn: '1h' },
     (err: Error | null, token?: string) => {
       if (err || !token) {
         return res.status(500).json({ error: 'Token generation failed' });
