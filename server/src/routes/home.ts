797e77e0c055a2a18eeae857a5011929140bbad9
Dainis Dilevka <thedenisovan@proton.me>
2026-01-27 12:49:01 +0000

    add: conditional to display published/unpublished posts

--- server/src/routes/home.ts
+++ server/src/routes/home.ts
@@ -4,7 +4,15 @@ import verifyToken from '../middleware/verifyToken.js';
 
 const home = Router();
 
-home.get('/', getAllPublishedPosts);
+home.get('/', (req, res, next) => {
+  // If request includes all parameter return all posts included unpublished ones
+  if (req.query.posts === 'all') {
+    return getAllPublishedPosts(req, res, false);
+    // Else return only published posts
+  } else {
+    return getAllPublishedPosts(req, res);
+  }
+});
 home.post('/', verifyToken);
 
 export default home;
