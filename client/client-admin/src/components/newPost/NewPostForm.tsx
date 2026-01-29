e115c01cfad75726b34ad9150882a4f5ad1a9719
Dainis Dilevka <thedenisovan@proton.me>
2026-01-29 12:00:59 +0000

    add: form for creating new posts with post api

--- client/client-admin/src/components/newPost/NewPostForm.tsx
+++ client/client-admin/src/components/newPost/NewPostForm.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, type FormEvent } from 'react';
-import { useOutletContext, Link } from 'react-router';
+import { useOutletContext, Link, Navigate } from 'react-router';
 
 export default function NewPostForm() {
   const { isAdmin } = useOutletContext<{ isAdmin: boolean }>();
@@ -8,14 +8,16 @@ export default function NewPostForm() {
   const [title, setTitle] = useState<string>('');
   const [content, setContent] = useState<string>('');
   const [description, setDescription] = useState<string>('');
-  const [published, setPublished] = useState<boolean>(false);
-  const [categoryId, setCategoryId] = useState<number | null>(null);
-  const [categoryName, setCategoryName] = useState<string | null>(null);
+  const [categoryId, setCategoryId] = useState<number>(0);
+  const [categoryName, setCategoryName] = useState<string>('');
   const [token, setToken] = useState<string>('');
+  const [isPostCreated, setPostCreated] = useState<boolean>(false);
 
+  // After page loads checks if user is admin if yes get
+  // his token and get his id
   useEffect(() => {
     const getAuthorId = () => {
-      const author = localStorage.getItem('author');
+      const author = localStorage.getItem('user');
       if (author) setAuthorId(JSON.parse(author).id);
     };
 
@@ -32,19 +34,19 @@ export default function NewPostForm() {
     }
   });
 
+  // Create new post api
   const submitNewPost = async (
     e: FormEvent,
     title: string,
     content: string,
     description: string,
-    published: boolean,
-    categoryId?: number | null,
-    categoryName?: string | null,
+    categoryId?: number,
+    categoryName?: string,
   ) => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localstorage:5000/newPost', {
+      const response = await fetch('http://localhost:5000/newPost', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -53,25 +55,25 @@ export default function NewPostForm() {
         body: JSON.stringify({
           authorId,
           title,
-          content,
-          description,
-          published,
           categoryId,
           categoryName,
+          content,
+          description,
         }),
       });
 
       if (!response.ok) throw new Error(`Error ${response.status}`);
 
-      const result = await response.json();
-
-      console.log(result);
+      setPostCreated(true);
     } catch (error) {
       console.error(
         `Error occur during posting new post ${error instanceof Error ? error.message : String(error)}`,
       );
     }
   };
+  if (isPostCreated) {
+    return <Navigate to='/' />;
+  }
 
   return (
     <main className='bg-slate-900 min-h-screen'>
@@ -98,19 +100,126 @@ export default function NewPostForm() {
           </svg>
           Cancel
         </Link>
-        <form
-          onSubmit={(e) =>
-            submitNewPost(
-              e,
-              title,
-              content,
-              description,
-              published,
-              categoryId,
-              categoryName,
-            )
-          }
-        ></form>
+        <div className='bg-slate-800 border border-slate-700 rounded-lg p-8'>
+          <h2 className='text-2xl font-bold text-white mb-6!'>
+            Create New Post
+          </h2>
+          <form
+            className='space-y-6!'
+            onSubmit={(e) =>
+              submitNewPost(
+                e,
+                title,
+                content,
+                description,
+                categoryId,
+                categoryName,
+              )
+            }
+          >
+            <div>
+              <label
+                htmlFor='title'
+                className='block text-md font-medium text-gray-300 mb-2!'
+              >
+                Title
+              </label>
+              <input
+                type='text'
+                name='title'
+                id='title'
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                required
+                className='w-full  px-4 py-2 text-md bg-slate-900 border border-slate-600 rounded-md focus-ring2 focus:ring-slate-500 focus:border-transparent outline-none text-white'
+              />
+            </div>
+            <div>
+              <label
+                htmlFor='categoryName'
+                className='block text-md font-medium text-gray-300 mb-2!'
+              >
+                New Category Name{' '}
+                <span className='text-green-400'>
+                  *(if fill field this skip category id field)
+                </span>
+              </label>
+              <input
+                type='text'
+                name='categoryName'
+                id='categoryName'
+                value={categoryName}
+                onChange={(e) => setCategoryName(e.target.value)}
+                required={!categoryId ? false : true}
+                readOnly={!categoryId ? false : true}
+                className='w-full  px-4 py-2 text-md bg-slate-900 border border-slate-600 rounded-md focus-ring2 focus:ring-slate-500 focus:border-transparent outline-none text-white'
+              />
+            </div>
+            <div>
+              <label
+                htmlFor='categoryId'
+                className='block text-md font-medium text-gray-300 mb-2!'
+              >
+                Category ID{' '}
+                <span className='text-green-400'>
+                  *(if fill field this skip category name field)
+                </span>
+              </label>
+              <input
+                type='number'
+                name='categoryId'
+                id='categoryId'
+                value={categoryId}
+                onChange={(e) => setCategoryId(Number(e.target.value))}
+                required={!categoryName ? false : true}
+                readOnly={!categoryName ? false : true}
+                className='w-full  px-4 py-2 text-md bg-slate-900 border border-slate-600 rounded-md focus-ring2 focus:ring-slate-500 focus:border-transparent outline-none text-white'
+              />
+            </div>
+            <div>
+              <label
+                htmlFor='description'
+                className='block text-md font-medium text-gray-300 mb-2!'
+              >
+                Description
+              </label>
+              <textarea
+                rows={3}
+                name='description'
+                id='description'
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                required
+                className='w-full  px-4 py-2 text-md bg-slate-900 border border-slate-600 rounded-md focus-ring2 focus:ring-slate-500 focus:border-transparent outline-none text-white'
+              ></textarea>
+            </div>
+            <div>
+              <label
+                htmlFor='content'
+                className='block text-md font-medium text-gray-300 mb-2!'
+              >
+                Content
+              </label>
+              <textarea
+                rows={15}
+                name='content'
+                id='content'
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                required
+                className='w-full  px-4 py-2 text-md bg-slate-900 border border-slate-600 rounded-md focus-ring2 focus:ring-slate-500 focus:border-transparent outline-none text-white'
+              ></textarea>
+            </div>
+            <div className='flex gap-4 pt-4'>
+              <button
+                type='submit'
+                className='bg-slate-700 text-white px-6 py-3 rounded-md font-medium hover:bg-slate-600 transition-colors'
+              >
+                Create Post
+              </button>
+            </div>
+          </form>
+        </div>
       </section>
     </main>
   );
